50a3fa893b035186a3b8ea6376fa29a31fde331f
stivan301 <stef_321@abv.bg>
2020-02-23 12:16:59 +0200

    vs cod

---
+++ ConsoleApp1/ConsoleApp1/Prodict.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Prodict
+    {
+        private int id;
+        private string name;
+        private decimal price;
+        private int stock;
+        private DateTime expiry;
+        public Prodict(int id,string name,decimal price,int stock,DateTime expiry)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+            this.Stock = stock;
+            this.Expiry = expiry;
+        }
+        public DateTime Expiry
+        {
+            get { return this.expiry; }
+            set { this.expiry = value; }
+        }
+        public int Stock
+        {
+            get { return this.stock; }
+            set { this.stock = value; }
+        }
+        public decimal Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prodict product = new Prodict(1, "Test product", 100.01m, 100, new DateTime(2019, 06, 30));
+            string json = JsonConvert.SerializeObject(product);
+            Console.WriteLine("Single product object");
+            Console.WriteLine(json);
+            Console.WriteLine(new string('-',50));
+        }
+    }
+}
